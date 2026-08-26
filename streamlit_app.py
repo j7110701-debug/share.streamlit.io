dcc611c6957e058c44540e1b9a7bdf9ead4215e0
j7110701-debug <j7110701@gmail.com>
2026-08-26 20:06:42 +0000

    update model

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,56 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+Oh no.
+Error running app. If you need help, try the Streamlit docs and forums.
+
+[     UTC     ] Logs for blank-app-l6x8ts62cta.streamlit.app/
+
+────────────────────────────────────────────────────────────────────────────────────────
+
+[18:05:33] 🚀 Starting up repository: 'conspiracy.chat.bot', branch: 'main', main module: 'streamlit_app.py'
+
+[18:05:33] 🐙 Cloning repository...
+
+[18:05:34] 🐙 Cloning into '/mount/src/conspiracy.chat.bot'...
+
+[18:05:34] 🐙 Cloned repository!
+
+[18:05:34] 🐙 Pulling code changes from Github...
+
+[18:05:34] ❗️ The main module file does not exist: /mount/src/conspiracy.chat.bot/streamlit_app.py
+
+[18:05:34] ❗️ Error during processing dependencies! Please fix the error and push an update, or try restarting the app.
+
+[18:06:36] 🖥 Provisioning machine...
+
+[18:06:42] 🎛 Preparing system...
+
+[18:06:43] ⛓ Spinning up manager process...
+
+[18:06:41] 🚀 Starting up repository: 'conspiracy.chat.bot', branch: 'main', main module: 'streamlit_app.py'
+
+[18:06:41] 🐙 Cloning repository...
+
+[18:06:41] 🐙 Cloning into '/mount/src/conspiracy.chat.bot'...
+
+[18:06:41] 🐙 Cloned repository!
+
+[18:06:41] 🐙 Pulling code changes from Github...
+
+[18:06:42] ❗️ The main module file does not exist: /mount/src/conspiracy.chat.bot/streamlit_app.py
+
+[18:06:42] ❗️ Error during processing dependencies! Please fix the error and push an update, or try restarting the app.
+
+main
+j7110701-debug/conspiracy.chat.bot/main/streamlit_app.py
+
+
+File "/mount/src/conspiracy.chat.ai/streamlit_app.py", line 14
+          if os.path.exists(path):
+                   ^
+                   IndentationError: unexpected indentstreamlit
+                   groq
+                   ollama
+                   requests
+                   
